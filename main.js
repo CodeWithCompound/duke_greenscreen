bccab7c7844ff987a9110f81db0b9192e88af5b0
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-17 14:15:07 +0100

    setting up the plan for the project

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // greenscreen: also known as chroma key
 // this effect removes a specific color from the video feed
 // typically used to replace a background with another image or video
+/*
+ STEPS FOR ALGORYITHM: (generalized for any algorithm)
+ 1. Work Example out by hand
+ 2. Write down what you did
+ 3. Find patterns
+ 4. Check by hand
+ 5. Translate to code
+ 6. Test and debug
+ 7. Optimize if necessary
+*/
