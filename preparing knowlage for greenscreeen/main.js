1d56453049765155ed7e156d69e9028e204c0d12
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-18 08:51:08 +0100

    two separate project folders for seperation of learning & the project

--- preparing knowlage for greenscreeen/main.js
+++ preparing knowlage for greenscreeen/main.js
@@ -1,6 +1,7 @@
 // greenscreen: also known as chroma key
 // this effect removes a specific color from the video feed
 // typically used to replace a background with another image or video
+
 /*
  STEPS FOR ALGORYITHM: (generalized for any algorithm)
  1. Work Example out by hand
@@ -11,3 +12,24 @@
  6. Test and debug
  7. Optimize if necessary
 */
+
+var x = 3;
+var y = 3 * 3;
+
+for (var i = 0; i < y; i++) {
+  if (i == 0) {
+    console.log("starting the loop");
+  }
+  console.log(i);
+  if (i == y - 1) {
+    console.log("done with the loop iteration");
+  }
+}
+
+console.log(y);
+function sqare(n) {
+  return n * n;
+}
+y = sqare(5);
+console.log(y);
+console.log(sqare(10));
